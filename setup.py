417f6e6950fb9ed306c486c822aa3fa0adb3f477
(DHSC) Annabel Westermann <Annabel.Westermann@phe.gov.uk>
2024-02-07 09:56:32 +0000

    'initialise'

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='ph_statistical_methods',
+    version='0.1.1',
+    packages=['ph_statistical_methods'],
+    url='https://gitlab.phe.gov.uk/Russell.Plunkett/phe-indicator-methods.git',
+    license='',
+    author='Department of health and Social Care',
+    author_email='russell.plunkett@phe.gov.uk, phds@phe.gov.uk',
+    description='This is a python package to calculate outputs used in the production of indicators within PHE.',
+    install_requires=['pandas>=1.5', 'scipy', 'numpy', 'pytest']
+)
