0ee5d1f6d8917c846a493a156996d150312bde31
(DHSC) Annabel Westermann <Annabel.Westermann@phe.gov.uk>
2024-03-07 19:06:19 +0000

    google docstrings

--- ph_statistical_methods/proportions.py
+++ ph_statistical_methods/proportions.py
@@ -7,21 +7,40 @@ Created on Thu Feb 22 16:52:40 2024
 
 import pandas as pd
 
-from ph_statistical_methods.confidence_intervals import wilson_lower, wilson_upper
-from ph_statistical_methods.validation import metadata_cols, ci_col, validate_data
+from confidence_intervals import wilson_lower, wilson_upper
+from validation import metadata_cols, ci_col, validate_data
 
-#df = pd.read_excel('ph_statistical_methods/unit_tests/test_data/testdata_Proportion.xlsx')
+#df = pd.read_excel('unit_tests/test_data/testdata_Proportion.xlsx')
 
-df = pd.DataFrame({'area': ['Area1', 'Area2', 'Area3','Area4']*3,
-                  'num': [None, 82, 9, 48, 65, 8200, 10000, 10000, 8, 7, 750, 900],
-                  'den': [100, 10000, 10000, 10000] * 3})
+# df = pd.DataFrame({'area': [1, 2]*6,
+#                    'area2': ['Area7', 'Area2','Area1']*4,
+#                   'num': [None, 82, 9, 48, 65, 8200, 10000, 10000, 8, 7, 750, 900],
+#                   'den': [100, 10000, 10000, 10000] * 3})
 
 
         
-def ph_proportion(df, num_col, denom_col, metadata = True, confidence = 0.95, multiplier = 1):
-    
+def ph_proportion(df, num_col, denom_col, group_cols = [], metadata = True, confidence = 0.95, multiplier = 1):
+    """Calculates proportions with confidence limits using Wilson Score method.
+
+    Args:
+        df: DataFrame containing the data to calculate proportions for.
+        num_col (str): Name of column containing observed number of cases in the sample
+                (the numerator of the population).
+        denom_col (str): Name of column containing number of cases in sample 
+                (the denominator of the population).
+        group_cols (list): A list of column name(s) to group the data by. 
+                Defaults to an empty list, to not group data.
+        metadata (bool): Whether to include information on the statistic and confidence interval methods.
+        confidence: Confidence interval(s) to use, either as a float, list of float values or None.
+                Confidence intervals must be between 0.9 and 1. Defaults to 0.95 (2 std from mean).
+        multiplier (int): multiplier used to express the final values (e.g. 100 = percentage)
+
+    Returns:
+        DataFrame of calculated proportion statistics with confidence intervals.
+        
+    """
     # Check data and arguments
-    confidence = validate_data(df, [num_col, denom_col], confidence, metadata)
+    confidence = validate_data(df, [num_col, denom_col], group_cols, confidence, metadata)
     
     if (df[num_col] > df[denom_col]).any():
         raise ValueError('Numerators must be less than or equal to the denominator for a proportion statistic')
@@ -29,8 +48,12 @@ def ph_proportion(df, num_col, denom_col, metadata = True, confidence = 0.95, mu
     if not isinstance(multiplier, int):
         raise TypeError("'Multiplier' must be an integer")
     
+    # this ignores the NA whereas R version keeps it as NA?
+    if len(group_cols) > 0:
+        df = df.groupby(group_cols)[[num_col, denom_col]].sum().reset_index()
+        
     ### Calculate statistic
-    df['Value'] = (df['num_col'] / df['denom_col']) * multiplier
+    df['Value'] = (df[num_col] / df[denom_col]) * multiplier
 
     if confidence is not None:
         for c in confidence:
