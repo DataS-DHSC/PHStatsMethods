93bfce8c9201144002e1cdeb013f4aa8cc7b8656
(DHSC) Thilaksan Vikneswaran <T.Vikneswaran_DHSC@phe.gov.uk>
2024-03-13 12:28:11 +0000

    Created funnel ratio significance code

--- ph_statistical_methods/utils.py
+++ ph_statistical_methods/utils.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from scipy.special import ndtri
-
+import math
+from scipy import stats 
+import pandas as pd
+import numpy as np
 
 def get_calc_variables(a):
     """
@@ -80,5 +83,48 @@ def poisson_funnel(obs, p, side):
     p_funnel = (1 + obs) * v / (1 - v)
     return p_funnel
 
+
+
+def funnel_ratio_significance(obs, expected, p, side):
+    """
+    Calculate funnel ratio significance for given observations, expected value, probability, and side.
+    :param obs: Observations, integer.
+    :param expected: Expected value, float.
+    :param p: Probability, float.
+    :param side: Side, 'high' or 'low'.
+    :return: Test statistic, float.
+    """
+    # Special case handling when observation is 0 and considering the lower side
+    if obs == 0 and side == "low":
+        test_statistic = 0
+        
+    # For small sample sizes (less than 10)
+    elif obs < 10:
+        if side == "low":
+            degree_freedom = 2 * obs
+            lower_tail_setting = False
+        elif side == "high":
+            degree_freedom = 2 * obs + 2
+            lower_tail_setting = True
+            
+        # Chi-squared test statistic calculation
+        if lower_tail_setting:
+            test_statistic = stats.chi2.ppf(0.5 + p / 2, df=degree_freedom) / 2
+        else:
+            test_statistic = stats.chi2.ppf(1 - (0.5 + p / 2), df=degree_freedom) / 2
+
+    # For larger sample sizes (10 or more)
+    else:
+        if side == "low":
+            obs_adjusted = obs
+            z = stats.norm.ppf(0.5 + p / 2)
+            test_statistic = obs_adjusted * (1 - 1 / (9 * obs_adjusted) - z / 3 / np.sqrt(obs_adjusted))**3
+        elif side == "high":
+            obs_adjusted = obs + 1
+            z = stats.norm.ppf(0.5 + p / 2)
+            test_statistic = obs_adjusted * (1 - 1 / (9 * obs_adjusted) + z / 3 / np.sqrt(obs_adjusted))**3
+
+    test_statistic = test_statistic / expected
+    return test_statistic
 
 
