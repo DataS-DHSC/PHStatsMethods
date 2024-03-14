176f21327f86c1b4e4a8a8d13d04536d7882ce71
annabelwestermann96 <110179376+annabelwestermann96@users.noreply.github.com>
2024-03-14 17:05:37 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Department of health and Social Care',
     author_email='annabel.westermann@dhsc.gov.uk, phds@phe.gov.uk',
     description='This is a python package to calculate statistics in public health, including indicators for Fingertips.',
-    install_requires=['numpy >= 1.25.'0,
+    install_requires=['numpy >= 1.25.0',
                       'pandas >= 2.0.0',
                       'pytest >= 8.0.0',
                       'scipy >= 1.8.0']
