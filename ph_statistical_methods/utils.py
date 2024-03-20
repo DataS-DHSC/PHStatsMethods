0c9f766a488ba08e8a8073d3e912d0aad9f2897f
(DHSC) Annabel Westermann <Annabel.Westermann@phe.gov.uk>
2024-03-20 17:06:04 +0000

    adjust cis

--- ph_statistical_methods/utils.py
+++ ph_statistical_methods/utils.py
@@ -62,31 +62,4 @@ def poisson_funnel(obs, p, side):
     return p_funnel
 
 
-def exact_lowercl(numerator, denominator, confidence = 0.95, multiplier = 100000):
-    """
-    Calculates exact lower confidence interval, takes in numerator, denominator, confidence, and multiplier.
-    :param numerator:  numerator value, input as integer
-    :param denominator: denomintaor value, input as integer
-    :param confidence: confidence level used for calculation, set as default 0.95 for 95% confidence level.
-    :param multiplier: multiplier level used for calulcation, default as 100000.
-
-    """
-   #calculates lower confidence interval
-    lowercl = chi2(1-confidence)/2, 2*numerator/2/denominator * multiplier
-    return lowercl
-
-def exact_uppercl(numerator, denominator, confidence = 0.95, multiplier = 100000):
-    """
-    Calculates exact upper confidence interval, takes in numerator, denominator, confidence, and multiplier.
-    :param numerator:  numerator value, input as integer
-    :param denominator: denomintaor value, input as integer
-    :param confidence: confidence level used for calculation, set as default 0.95 for 95% confidence level.
-    :param multiplier: multiplier level used for calulcation, default as 100000.
-
-    """
-   #calculates upper confidence interval
-    uppercl = chi2(confidence+(1-confidence)/2,2*numerator/2/denominator * multiplier)
-    return uppercl
-
-
 
