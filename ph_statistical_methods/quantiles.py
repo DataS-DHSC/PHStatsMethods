2cbc2b68a1c991bc527d8ce08e860c7210804e52
HadleyNanayakkara <hadley10@hotmail.co.uk>
2024-04-29 16:50:55 +0100

    quantiles function

--- ph_statistical_methods/quantiles.py
+++ ph_statistical_methods/quantiles.py
@@ -6,6 +6,7 @@ Created on Fri Apr 26 16:52:40 2024
 """
 
 import pandas as pd
+import numpy as np
 
 from .validation import format_args, check_arguments
 
@@ -19,9 +20,8 @@ def ph_quantile(df, values, group_cols = None, nquantiles = 10, invert = True, t
         group_cols: A string or list of column name(s) to group the data by. 
                 Defaults to None.
         nquantiles (int): the number of quantiles to separate each grouping set into.
-        invert (bool) or (str): whether the quantiles should be directly (False) or inversely (True) related
-                    to the numerical value order OR unquoted string referencing field name from data that 
-                    stores logical values for each grouping set.
+        invert (bool): whether the quantiles should be directly (False) or inversely (True) related
+                    to the numerical value order.
         type (str): defines whether to include metadata columns in output to reference the arguments 
                     passed; can be "standard" or "full".
 
@@ -34,7 +34,6 @@ def ph_quantile(df, values, group_cols = None, nquantiles = 10, invert = True, t
     """
 
     # Check data and arguments
-    cols = values
     null, group_cols = format_args(None, group_cols)
 
     if group_cols is not None:
@@ -47,8 +46,34 @@ def ph_quantile(df, values, group_cols = None, nquantiles = 10, invert = True, t
     if not isinstance(nquantiles, int):
         raise TypeError("Pass 'nquantiles' as a integer")
     
-    if not isinstance(invert, (bool, str))):
+    if not isinstance(invert, bool):
         raise TypeError("Pass 'invert' as either a boolean or a string")
     
-    if isinstance(invert, str): cols += invert
-    check_arguments(df, cols if group_cols is None else cols + group_cols)
+    check_arguments(df, values if group_cols is None else values + group_cols)
+    
+    # Additional columns in output
+    df["nquantiles"] = nquantiles
+
+    # Calculate Quantiles 
+    if group_cols is None:
+        df['num_rows'] = df[values].count() # Number of rows in total
+        df['rank'] = df[values].rank(ascending = not invert, method='min') # Rank each value
+    else:   
+        df['num_rows'] = df.groupby(group_cols)[values].transform(lambda x: x.count()) # Number of rows in each group
+        df['rank'] = df.groupby(group_cols)[values].rank(ascending = not invert, method='min') # Rank each value in each group
+
+
+    # Assign a quantile based on rank and number of rows in each group 
+    df['quantile'] = np.where(df['num_rows'] < nquantiles,
+                              np.nan,
+                              np.floor((nquantiles + 1) - np.ceil(((df['num_rows'] + 1) - df['rank']) / (df['num_rows'] / nquantiles))))
+
+    if invert is True:
+        df['qinverted'] = "lowest quantile represents highest values"
+    else:
+        df['qinverted'] = "lowest quantile represents lowest values"
+
+    if type is "standard":
+        df = df.drop(['num_rows', 'rank', 'nquantiles', 'qinverted'], axis=1)
+
+    return(df)
