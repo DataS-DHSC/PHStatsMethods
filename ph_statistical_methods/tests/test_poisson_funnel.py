a1b9c9d5e6a64a8bc14f80897f6d8ab81d07a4f1
(DHSC) Jack Burden <Jack.Burden_DHSC@phe.gov.uk>
2024-05-01 09:52:22 +0100

    test started

---
+++ ph_statistical_methods/tests/test_poisson_funnel.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  1 08:18:14 2024
+
+@author: Jack.Burden_DHSC
+"""
+
+import pytest
+from utils_funnel import poisson_funnel
+
+@pytest.mark.parametrize('obs, p, side, result', 
+
+def test_poisson_funnel(obs, p, side, result):
