8c1303b4e07be708fa287b77b138f4a50c0c7c58
Hadley Nanayakkara <hadley10@hotmail.co.uk>
2024-05-08 11:41:44 +0100

    relative imports

--- ph_statistical_methods/__init__.py
+++ ph_statistical_methods/__init__.py
@@ -1,6 +1,6 @@
 # External Functions
-from confidence_intervals import wilson, exact, byars, poisson_cis
-from ISRatio import calculate_ISRatio
-from means import ph_mean
-from proportions import ph_proportion
-from rates import ph_rate
+from .confidence_intervals import wilson, exact, byars, poisson_cis
+from .ISRatio import calculate_ISRatio
+from .means import ph_mean
+from .proportions import ph_proportion
+from .rates import ph_rate
