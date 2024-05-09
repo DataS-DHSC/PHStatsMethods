9c0cbf40da44ba9fd86d7b5bb38dfc5003089c55
Cameron Stewart <cameron.stewart@dhsc.gov.uk>
2024-05-09 09:38:56 +0100

    First draft of findxvalues, issues with output to be resolved

--- ph_statistical_methods/utils.py
+++ ph_statistical_methods/utils.py
@@ -2,6 +2,8 @@
 
 from scipy.special import ndtri
 from scipy.stats import chi2
+import pandas as pd
+import numpy as np
 
 def get_calc_variables(a):
     """
@@ -13,6 +15,36 @@ def get_calc_variables(a):
     norm_cum_dist = ndtri((100 + (100 - (100 * (1-a)))) / 200)
     z = ndtri(1 - (1-a )/ 2)
     return norm_cum_dist, z
+
+def findxvalues(xvals, no_quantiles):
+    """Calculates mid-points of cumulative population for each quantile.
+    
+    Args:
+        xvals: field name in input Dataset that contains the quantile populations
+        no_quantiles: (integer) number of quantiles supplied in dataset for SII
+        
+        Returns:
+            Output field of mid-points
+            
+    """
+    df = pd.DataFrame({'prop': [0.0] * no_quantiles,
+                       'cumprop': [0.0] * no_quantiles,
+                       'output': [0.0] * no_quantiles})
+
+    xvals = np.array(xvals)
+    
+    df['prop'] = xvals / sum(xvals)
+    df['cumprop'] = df['prop'].cumsum()
+    df['output'] = #np.where(df['cumprop'].shift(1).isna(), df['prop'] / 2, df['prop'] / 2 + df['cumprop'].fillna(0) / 2)
+    
+    return df['output']
+
+
+    
+    
+    
+    
+    
 
 
 
