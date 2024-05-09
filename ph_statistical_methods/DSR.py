f7e5a18e008dbd2b3465db5cf2f33934724972f0
(DHSC) Annabel Westermann <Annabel.Westermann@phe.gov.uk>
2024-05-09 20:27:44 +0100

    dsr

--- ph_statistical_methods/DSR.py
+++ ph_statistical_methods/DSR.py
@@ -2,9 +2,47 @@
 
 import pandas as pd
 
-from utils import join_age_groups
+from utils import join_euro_standard_pops
 from validation import format_args, validate_data, check_kwargs
 
 
 
+df = pd.DataFrame({'year': [2017] * 19 + [2018]*19 + [2019]*19 + [2020]*19, 
+                   'ageband': [0, 5, 10, 15, 20, 25, 30, 35, 40, 45,
+                               50, 55, 60, 65, 70, 75, 80, 85, 90] * 4,
+                   'obs': [100, 300, 250, 400, 350] * 15 + [380],
+                   'pop': [1000, 3000, 900, 1200, 2000] * 15 + [900]})
 
+group_cols = 'year'
+confidence = 0.95
+denom_col = 'pop'
+num_col = 'obs'
+
+def ph_dsr(df, num_col, denom_col, ref_denom_col, group_cols = None, metadata = True, 
+           confidence = 0.95, multiplier = 100000, euro_standard_pops = True, **kwargs)
+
+df = df.copy().reset_index()
+
+confidence, group_cols = format_args(confidence, group_cols)
+ref_df, ref_join_left, ref_join_right = check_kwargs(df, kwargs, 'ref', ref_denom_col)
+validate_data(df, num_col, group_cols, metadata, denom_col, ref_df = ref_df)
+
+# Join reference data
+if euro_standard_pops:
+    df = join_euro_standard_pops(df, ref_denom_col, group_cols)
+    ref_denom_col = 'euro_standard_pops'
+    
+elif ref_df is not None:
+    df = df.merge(ref_df, on = 'left', left_on = ref_join_left, right_on = ref_join_right)
+    
+df['wt_rate'] = df[num_col].fillna(0) * df[ref_denom_col] / df[denom_col]
+df['sq_rate'] = df[num_col].fillna(0) * (df[ref_denom_col] / df[denom_col]**2)
+
+dsr = df.groupby(group_cols).agg({num_col: 'sum', 
+                                  denom_col: lambda x: x.sum(skipna=False), 
+                                  'wt_rate': lambda x: x.sum(skipna=False),
+                                  ref_denom_col: lambda x: x.sum(skipna=False), 
+                                  'sq_rate': lambda x: x.sum(skipna=False)})
+
+dsr['Value'] = dsr['wt_rate'] / dsr[ref_denom_col] * multiplier
+dsr['vardsr'] = 1 / dsr[ref_denom_col]**2 * dsr['sq_rate']
--- ph_statistical_methods/utils.py
+++ ph_statistical_methods/utils.py
@@ -19,15 +19,15 @@ def get_calc_variables(a):
 
 def euro_standard_pop():
     
-    esp_age_bands = ['0-4', '5-9', '10-14', '15-19', '20-24', '25-29', '30-34',
+    age_groups = ['0-4', '5-9', '10-14', '15-19', '20-24', '25-29', '30-34',
                   '35-39', '40-44', '45-49', '50-54', '55-59', '60-64',
                   '65-69', '70-74', '75-79', '80-84', '85-89', '90+']
     
     pops = [5000, 5500, 5500, 5500, 6000, 6000, 6500, 7000, 7000, 7000, 
             7000, 6500, 6000, 5500, 5000, 4000, 2500, 1500, 1000]
     
-    data = pd.DataFrame({'age_group': age_groups,
-                         'populations': pops})
+    data = pd.DataFrame({'esp_age_bands': age_groups,
+                         'euro_standard_pops': pops})
     
     return data
 
@@ -45,17 +45,17 @@ def join_euro_standard_pops(df, age_col, group_cols = None):
             raise ValueError('There must be 19 rows of data per group')
             
     else:
-        if len(df) != len(age_df):
-            raise ValueError('Dataframe, if ungrouped, must have same number of rows as reference data')
+        if len(df) != 19:
+            raise ValueError('Dataframe, if ungrouped, must have 19 rows')
     
     # Get euro standard pops and rank order
     esp = euro_standard_pop()
-    esp['n1'] = esp['esp_age_bands'].rank()
+    esp['n1'] = list(range(1, 20))
     
     # Get first number of age and sort ascending
-    df['n1'] = df.apply(lambda x: str(re.findall(r'(\d+)', x[col])[0]), axis=1)
+    df['n1'] = df.apply(lambda x: int(re.findall(r'(\d+)', str(x[age_col]))[0]), axis=1)
     
-    if df['n1'].nunique() == (len(df) if group_cols is None else n_group_rows.counts.unique()):
+    if df['n1'].nunique() != 19:
         raise ValueError('There are duplicate minimum ages, which is not accepted as the function orders by the first number in each age band.\
                          For example, <5 and 5-10 IS NOT accepted but <=4 and 5-10 IS accepted.')
     
@@ -67,7 +67,7 @@ def join_euro_standard_pops(df, age_col, group_cols = None):
         df['n1'] = df['n1'].rank()
     
     # join by columns
-    df = df.merge(age_df, how='left', on='n1').drop('n1', axis=1)
+    df = df.merge(esp, how='left', on='n1').drop('n1', axis=1)
     
     return df
     
