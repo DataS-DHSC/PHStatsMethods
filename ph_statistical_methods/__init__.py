969c99dd40efd788dc22173a50c079b30adc635c
Hadley Nanayakkara <hadley10@hotmail.co.uk>
2024-05-14 10:48:59 +0100

    updated init

--- ph_statistical_methods/__init__.py
+++ ph_statistical_methods/__init__.py
@@ -1,5 +1,6 @@
 # External Functions
-from .confidence_intervals import wilson, exact, byars, poisson_cis
+from .confidence_intervals import wilson, exact, byars, student_t_dist
+from .utils_funnel import poisson_cis
 from .ISRatio import calculate_ISRatio
 from .means import ph_mean
 from .proportions import ph_proportion
