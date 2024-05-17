e9a1c78c26fbcbc8648fbdc8f4f395dd106939e4
(DHSC) Annabel Westermann <Annabel.Westermann@phe.gov.uk>
2024-05-17 12:35:15 +0100

    fix prop

--- ph_statistical_methods/tests/test_proportions.py
+++ ph_statistical_methods/tests/test_proportions.py
@@ -20,6 +20,7 @@ class TestProportions:
 
     # Import data - remove last Multiplier column as not a function output - just used for Excel calculation
     path = Path(__file__).parent / 'test_data/testdata_Proportion.xlsx'
+    path = 'tests/test_data/testdata_Proportion.xlsx'
     
     data = pd.read_excel(path, sheet_name = 'testdata_Prop').iloc[:,:-1]
     data_group = pd.read_excel(path, sheet_name = 'testdata_Prop_g').iloc[:,:-1]
@@ -33,21 +34,21 @@ class TestProportions:
             ph_proportion(self.data, 'Numerator', 'Denominator', multiplier = multiplier)
 
     def test_default(self):
-        df = ph_proportion(self.data.iloc[:8, :3], 'Numerator', 'Denominator').drop(['Confidence'], axis=1)
+        df = ph_proportion(self.data.iloc[:8, :3], 'Numerator', 'Denominator', 'Area').drop(['Confidence'], axis=1)
         assert_frame_equal(df, self.data.iloc[:8, self.cols_95])
     
     def test_2ci(self):
-        df = ph_proportion(self.data.iloc[:8, :3], 'Numerator', 'Denominator', confidence = [0.95, 0.998])
+        df = ph_proportion(self.data.iloc[:8, :3], 'Numerator', 'Denominator', 'Area', confidence = [0.95, 0.998])
         assert_frame_equal(df, self.data.iloc[:8, :])
         
     def test_percentage(self):
-        df = ph_proportion(self.data.iloc[8:16, :3], 'Numerator', 'Denominator', multiplier = 100)\
+        df = ph_proportion(self.data.iloc[8:16, :3], 'Numerator', 'Denominator', 'Area', multiplier = 100)\
             .drop(['Confidence'], axis=1)
-        assert_frame_equal(df, self.data.iloc[8:16, self.cols_95])
+        assert_frame_equal(df, self.data.iloc[8:16, self.cols_95].reset_index(drop=True))
         
     def test_NAs(self):
-        df = ph_proportion(self.data.iloc[16:, :3], 'Numerator', 'Denominator').drop(['Confidence'], axis=1)
-        assert_frame_equal(df, self.data.iloc[16:, self.cols_95])
+        df = ph_proportion(self.data.iloc[16:, :3], 'Numerator', 'Denominator', 'Area').drop(['Confidence'], axis=1)
+        assert_frame_equal(df, self.data.iloc[16:, self.cols_95].reset_index(drop=True))
         
     def test_group(self):
         df = ph_proportion(self.data, 'Numerator', 'Denominator', group_cols = 'Area')
