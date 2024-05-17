97bc18910663f927f393d819bef3798b39ecb379
(DHSC) Annabel Westermann <Annabel.Westermann@phe.gov.uk>
2024-05-17 09:55:09 +0100

    changes

--- ph_statistical_methods/__init__.py
+++ ph_statistical_methods/__init__.py
@@ -1,6 +1,7 @@
 # External Functions
 from .confidence_intervals import wilson, exact, byars, student_t_dist
-from .utils_funnel import poisson_cis
+from .utils_funnel import sigma_adjustment, poisson_funnel, funnel_ratio_significance
+from .funnels import calculate_funnel_limits, assign_funnel_significance, calculate_funnel_points
 from .DSR import ph_dsr
 from .ISRatio import ph_ISRatio
 from .ISRate import ph_ISRate
