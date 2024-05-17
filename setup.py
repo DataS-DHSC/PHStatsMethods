e1ba0714a400062f9b99b0c806b61e2d9cd80262
annabelwestermann96 <110179376+annabelwestermann96@users.noreply.github.com>
2024-05-17 15:45:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup
 
-
 setup(
     name='PHStatsMethods',
-    version='0.1.4',
+    version='0.1.5',
     packages=['PHStatsMethods'],
     url='https://github.com/DataS-DHSC/PHStatsMethods',
     license='GPL3',
     author='Department of Health and Social Care',
-    author_email='annabel.westermann@dhsc.gov.uk, phds@phe.gov.uk',
+    author_email='annabel.westermann@dhsc.gov.uk, hadley.nanayakkara@dhsc.gov.uk, cameron.stewart@dhsc.gov.uk, jack.burden@dhsc.gov.uk, thilaksan.vikneswaran@dhsc.gov.uk, paul.fryers@dhsc.gov.uk, phds@phe.gov.uk',
     description='This is a python package to calculate statistics in public health, including indicators for Fingertips.',
-    long_description='longer description',
+    long_description='This is a python package to calculate statistics in public health, including indicators for Fingertips.',
     install_requires=['numpy >= 1.24.0',
                       'pandas >= 2.0.0',
                       'pytest >= 8.0.0',
