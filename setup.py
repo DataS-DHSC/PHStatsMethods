c36c259bab514cfd4a3bb41cf3b1cce56278fbea
annabelwestermann96 <110179376+annabelwestermann96@users.noreply.github.com>
2024-05-17 14:21:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
                       'pandas >= 2.0.0',
                       'pytest >= 8.0.0',
                       'scipy >= 1.8.0',
-                      'openpyxl >= 3.1.0']
+                      'openpyxl >= 3.1.0'],
 )
 
