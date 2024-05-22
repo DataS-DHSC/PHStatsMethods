4cb350bfab8b3ee74d4c0c679e8efb95b9f64f49
Hadley Nanayakkara <112709236+HadleyNanayakkara@users.noreply.github.com>
2024-05-22 11:04:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Department of Health and Social Care',
     author_email='annabel.westermann@dhsc.gov.uk, hadley.nanayakkara@dhsc.gov.uk, cameron.stewart@dhsc.gov.uk, jack.burden@dhsc.gov.uk, thilaksan.vikneswaran@dhsc.gov.uk, paul.fryers@dhsc.gov.uk, karandeep.kaur@dhsc.gov.uk, phds@phe.gov.uk',
     description='This is a python package to calculate statistics in public health, including indicators for Fingertips.',
-    long_description=file: readme.md,
+    long_description=open("readme.md", 'r').read(),
     long_description_content_type='text/markdown',
     install_requires=['numpy >= 1.24.0',
                       'pandas >= 2.0.0',
