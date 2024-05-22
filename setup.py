9a6b76fd8ab0a56e0be366b15320a5ac1a07bc08
Hadley Nanayakkara <112709236+HadleyNanayakkara@users.noreply.github.com>
2024-05-22 10:46:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "readme.md").read_text()
 
 setup(
     name='PHStatsMethods',
-    version='0.1.4',
+    version='0.1.5',
     packages=['PHStatsMethods'],
     url='https://github.com/DataS-DHSC/PHStatsMethods',
-    license='GPL3',
+    license='GPLv3',
     author='Department of Health and Social Care',
     author_email='annabel.westermann@dhsc.gov.uk, phds@phe.gov.uk',
-    description='This is a python package to calculate statistics in public health, including indicators for Fingertips.',
-    long_description='longer description',
+    description=long_description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['numpy >= 1.24.0',
                       'pandas >= 2.0.0',
                       'pytest >= 8.0.0',
