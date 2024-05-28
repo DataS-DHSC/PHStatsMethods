f8d67f0f620e53ee9ee7970355ec8f540ac1e77e
Hadley Nanayakkara <hadley10@hotmail.co.uk>
2024-05-28 16:42:05 +0100

    Updated intro docstring

--- PHStatsMethods/__init__.py
+++ PHStatsMethods/__init__.py
@@ -1,4 +1,9 @@
 """
+PHStatsMethods
+==============
+
+Introduction
+------------
 This is a Python package to support analysts in the execution of statistical
 methods approved for use in the production of Public Health indicators such as
 those presented via `Fingertips <https://fingertips.phe.org.uk/>`__. It
@@ -10,27 +15,28 @@ Any feedback would be appreciated and can be provided using the Issues
 section of the `PHStatsMethods GitHub
 repository <https://github.com/DataS-DHSC/PHStatsMethods/issue>`__.
 
-Licence:
-    This project is released under the `GPL-3 <https://opensource.org/licenses/GPL-3.0>`__
-    licence.
+Licence
+-------
+This project is released under the `GPL-3 <https://opensource.org/licenses/GPL-3.0>`__
+licence.
 
-Examples: 
-    Below is a example using the ph_proportion() function to demonstrate the purpose of
-    package. 
+Examples
+--------
+Below is a example using the ph_proportion() function to demonstrate the purpose of
+package. 
 
-    >>> df = pd.DataFrame({'area': ["Area1", "Area2", "Area3", "Area4"] * 3,
-                  'numerator': [None, 48, 10000, 7, 82, 6500, 10000, 750, 9, 8200, 8, 900],
-                  'denominator': [100, 10000, 10000, 10000] * 3})
+>>> df = pd.DataFrame({'area': ["Area1", "Area2", "Area3", "Area4"] * 3,
+                       'numerator': [None, 48, 10000, 7, 82, 6500, 10000, 750, 9, 8200, 8, 900],
+                       'denominator': [100, 10000, 10000, 10000] * 3})
 
-    Ungrouped 
-    >>> PHStatsMethods.ph_proportion(df, 'numerator', 'denominator')
+Ungrouped 
+>>> PHStatsMethods.ph_proportion(df, 'numerator', 'denominator')
 
-    Grouped
-    >>> PHStatsMethods.ph_proportion(df, 'numerator', 'denominator', 'area')
+Grouped
+>>> PHStatsMethods.ph_proportion(df, 'numerator', 'denominator', 'area')
 
-    For more information on any function, you can use:
-
-    >>> help(PHStatsMethods.function)
+For more information on any function, you can use:
+>>> help(PHStatsMethods.function)
 
 """
 
