ae33fbc41692e405396a4fee02fe1715eeb0ca4f
Hadley Nanayakkara <112709236+HadleyBN@users.noreply.github.com>
2024-06-07 11:59:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='PHStatsMethods',
-    version='0.1.11',
+    version='0.1.12',
     packages=['PHStatsMethods'],
     url='https://github.com/DataS-DHSC/PHStatsMethods',
     license='GPL-3.0',
