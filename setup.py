4a4446322007c74633e82cfc5ca05201532470b5
Hadley Nanayakkara <112709236+HadleyBN@users.noreply.github.com>
2026-01-22 16:37:59 +0000

    Update setup.py
    Signed-off-by: Hadley Nanayakkara <112709236+HadleyBN@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='PHStatsMethods',
-    version='0.1.15',
+    version='0.1.16',
     packages=['PHStatsMethods'],
     url='https://github.com/dhsc-govuk/PHStatsMethods',
     license='GPL-3.0',
